a165066b769e21d3cf7430103369473aa2340a19
Basit715 <basitabass27411@gmail.com>
2024-08-08 22:08:32 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+     name = "DiamondPricePrediction",
+     author="Basit Pushoo",
+     author_email="basitabass27411@gmail.com",
+     packages=find_packages()
+)
