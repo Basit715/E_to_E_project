9e8a836cf210a996100dbcacaa4d5b003052c194
Basit715 <basitabass27411@gmail.com>
2024-08-08 13:53:18 +0530

    multiple files added

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+
+
+package_name = "DiamondPricePrediction"
+
+list_of_files = [
+     ".github/workflows/.gitkeep",
+     f"src/{package_name}/__init__.py",
+     f"src/{package_name}/components/__init__.py",
+     f"src/{package_name}/components/data_ingestion.py",
+     f"src/{package_name}/components/data_transformation.py",
+     f"src/{package_name}/components/model_trainer.py",
+     f"src/{package_name}/pipelines/__init__.py",
+     f"src/{package_name}/pipelines/training_pipeline.py",
+     f"src/{package_name}/pipelines/prediction_pipeline.py",
+     f"src/{package_name}/logger.py",
+     f"src/{package_name}/exception.py",
+     f"src/{package_name}/utils/__init__.py",
+     "notebooks/research.ipynb",
+     "notebooks/data/.gitkeep",
+     "requirements.txt",
+     "setup.py",
+
+     
+]
+
+
+for filepath in list_of_files:
+     filepath = Path(filepath)
+     fildedir,filename = os.path.split(filepath)
+     
+
+     if fildedir != "":
+          os.makedirs(fildedir,exist_ok=True)
+     
+     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)== 0):
+          with open(filepath, "w") as f:
+               pass
+     else:
+          print("file already exists")
+     
+     
+     
+     
+     
